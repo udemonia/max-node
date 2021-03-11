a987bff9fed771beea28d5a658907bbc40a23d81
udemonia <brandon.l.lambert@icloud.com>
2021-03-10 19:22:46 -0600

    Added: edit-product query

--- basic-node/app.js
+++ basic-node/app.js
@@ -1,14 +1,15 @@
 const path = require('path');
-
+const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(morgan('tiny'))
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
